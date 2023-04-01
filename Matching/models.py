beb02395aaa80cacfcd9bd176110a206f2367fd0
priyal-babel <priyal.babel@somaiya.edu>
2023-04-01 20:21:06 +0530

    Cards swipe done

--- Matching/models.py
+++ Matching/models.py
@@ -10,6 +10,5 @@ class Questions(models.Model):
 class Matches(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, default=None)
     matched_with = models.ForeignKey(User,related_name='matched_with_%(class)s_related',on_delete=models.CASCADE)
-    created_at = models.DateTimeField(auto_now_add=True)
     swiped_right = models.BooleanField(null=True, default=None)
     swiped_left = models.BooleanField(null=True, default=None)
