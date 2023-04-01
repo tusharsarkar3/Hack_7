cb27244acb457384459855740ff73394159de585
tusharsarkar3 <tushar.sarkar@somaiya.edu>
2023-04-01 23:32:55 +0530

    Matching Algorithm done

--- Matching/match_lago.py
+++ Matching/match_lago.py
@@ -34,20 +34,34 @@ def matching_algorithm(request):
             print("Consider them")
             final_matches.append(matches)
         number_of_similarities_movies = num_similarities_multi(user_answers.movie, matches.movie)
-        if number_of_similarities_movies >= Global_stats.binary_question_similarity_mean:
+        if number_of_similarities_movies >= Global_stats.movies_number_of_similarity_mean:
             print("Consider them")
             final_matches.append(matches)
 
         number_of_similarities_books = num_similarities_multi(user_answers.books, matches.books)
-        if number_of_similarities_books >= Global_stats.binary_question_similarity_mean:
+        if number_of_similarities_books >= Global_stats.books_number_of_similarity_mean:
             print("Consider them")
             final_matches.append(matches)
 
         number_of_similarities_music = num_similarities_multi(user_answers.music, matches.music)
-        if number_of_similarities_movies >= Global_stats.binary_question_similarity_mean:
+        if number_of_similarities_music >= Global_stats.music_number_of_similarity_mean:
             print("Consider them")
             final_matches.append(matches)
 
+        freq = {}
+        for item in matches:
+            if (item in freq):
+                freq[item] += 1
+            else:
+                freq[item] = 1
+
+        match_made_in_heaven = []
+        for key, value in  freq.items():
+            if value ==4:
+                match_made_in_heaven.append(key)
+        if len(match_made_in_heaven) == 0:
+            return None
+        return match_made_in_heaven
 
 
 
